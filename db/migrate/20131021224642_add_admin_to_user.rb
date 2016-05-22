6e7c342788f4e29db99e230421a0b39d1b821479
Alexander Lemkin <alexanderlemkin@gmail.com>
2016-05-22 13:54:51 -0400

    Removed files from previous version

--- db/migrate/20131021224642_add_admin_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddAdminToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :is_admin, :boolean
-  end
-end
--- spec/mailers/user_mailer_spec.rb
+++
@@ -1,6 +0,0 @@
-require "rails_helper"
-
-describe UserMailer, :type => :mailer do
-  # http://guides.rubyonrails.org/action_mailer_basics.html#mailer-testing
-  skip "add some examples to (or delete) #{__FILE__}"
-end
