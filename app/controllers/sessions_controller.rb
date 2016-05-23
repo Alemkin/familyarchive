de26b6ac4eb85afb28470d339560bf43f6a488c4
Alexander Lemkin <alexanderlemkin@gmail.com>
2016-05-23 19:28:40 -0400

    Implemented simple-form gem and changed logo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,13 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    user = User.find_by_email(params[:email].strip.downcase)
+    user = User.find_by_email(params[:sessions][:email].strip.downcase)
     # if the user exists AND the password entered is correct
-    if user && user.authenticate(params[:password])
+    if user && user.authenticate(params[:sessions][:password])
       # save the user id inside the browser cookie. This is how we keep the user logged in when they navigate around our website.
       session[:user_id] = user.id
       redirect_to '/'
