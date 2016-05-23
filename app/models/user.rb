2c15d854fcf61778cc17280532b41e0a130ca953
Alexander Lemkin <alexanderlemkin@gmail.com>
2016-05-23 19:03:05 -0400

    Added password field to before validation whitespace strip

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
-  before_validation :strip_whitespace, :only => [:name, :email]
+  before_validation :strip_whitespace, :only => [:name, :email, :password]
 
 private
 def strip_whitespace
