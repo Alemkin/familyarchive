e6902018ab5f84e2aa7f0f2a4e385c44eb576d62
Alexander Lemkin <alexanderlemkin@gmail.com>
2016-05-26 21:49:23 -0400

    Added validations to artifact model, and password length validation for user

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,3 +1,23 @@
 class Artifact < ActiveRecord::Base
+  validates_presence_of :artifact_name
+  validates_presence_of :short_description
+  validates_presence_of :story_text
+  validates_presence_of :image_url
+  validates_presence_of :subject_name
+  validates_presence_of :family_name
+  validates_presence_of :artifact_type
+
+  before_validation :strip_whitespace
+
+private
+def strip_whitespace
+  self.artifact_name = self.artifact_name.strip unless self.artifact_name.nil?
+  self.short_description = self.short_description.strip unless self.short_description.nil?
+  self.story_text = self.story_text.strip unless self.story_text.nil?
+  self.image_url = self.image_url.strip unless self.image_url.nil?
+  self.subject_name = self.subject_name.strip unless self.subject_name.nil?
+  self.family_name = self.family_name.strip unless self.family_name.nil?
+  self.artifact_type = self.artifact_type.strip unless self.artifact_type.nil?
+end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :email
   validates_presence_of :password
-  validates_uniqueness_of :email
+  validates_length_of :password, :minimum => 5, :maximum => 20, :allow_blank => false
+  validates_uniqueness_of :email, :case_sensitive => false
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
   before_validation :strip_whitespace, :only => [:name, :email, :password]
