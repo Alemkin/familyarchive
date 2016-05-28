2d206577fed338cac7fc8db247ec544b3f4c66ef
Alexander Lemkin <alexanderlemkin@gmail.com>
2016-05-28 19:40:21 -0400

    Have artifacts populatings from database in random order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
 
   def index
+    @artifacts = Artifact.order("RANDOM()").limit(50)
+    @styles = ["style1", "style2", "style3", "style4", "style5", "style6"]
+    @count = 0
   end
 
   def generic
