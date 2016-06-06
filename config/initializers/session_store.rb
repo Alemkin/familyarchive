139e57ad175e4ba01e7dbc4cc84ac951a7d73179
Alexander Lemkin <alexanderlemkin@gmail.com>
2016-06-06 16:05:47 -0400

    changed session to expire after 2 hours

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-FamilyArchive::Application.config.session_store :cookie_store, key: '_family_archive_session'
+FamilyArchive::Application.config.session_store :cookie_store,
+key: '_family_archive_session',
+expire_after: 2.hours
