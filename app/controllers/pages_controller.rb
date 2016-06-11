b1bd5f0a441724eb584ca3d4183c6bd9ab2c49f2
Alexander Lemkin <alexanderlemkin@gmail.com>
2016-06-11 17:30:51 -0400

    Fixed styling of search, added hover info and info if no results found

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
     @styles = ["style1", "style2", "style3", "style4", "style5", "style6"]
     @count = 0
     if params[:search]
-      @artifacts = Artifact.search(params[:search])
+      @artifacts = Artifact.search(params[:search]).order("RANDOM()").limit(24)
     else
       @artifacts = Artifact.order("RANDOM()").limit(24)
     end
